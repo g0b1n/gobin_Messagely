7d6c46a562416baeff4709f8335f727f73c91ed6
Gobin <gobin@gobinMacBook-Pro.local>
2024-02-06 14:09:41 -0500

    my first init

---
+++ express-messagely/routes/auth.js
@@ -0,0 +1,56 @@
+
+const jwt = require("jsonwebtoken");
+const Router = require("express").Router;
+const router = new Router();
+
+const User = require("../models/user");
+const { SECRET_KEY } = require("../config");
+const ExpressError = require("../expressError");
+const { authenticateJWT, ensureLoggedIn, ensureCorrectUser } = require('../middleware/auth');
+
+
+/** POST /login - login: {username, password} => {token}
+ *
+ * Make sure to update their last-login!
+ *
+ **/
+router.post("/login", async function (req, res, next) {
+    try {
+        let { username, password } = req.body;
+        if (await User.authenticate(username, password)) {
+            let token = jwt.sign({ username }, SECRET_KEY);
+            User.updateLoginTimestamp(username);
+            return res.json({ token });
+        } else {
+            throw new ExpressError("Invalid username/password", 400);
+        }
+    }
+
+    catch(e) {
+        return next(e);
+    }
+});
+
+/** POST /register - register user: registers, logs in, and returns token.
+ *
+ * {username, password, first_name, last_name, phone} => {token}.
+ *
+ *  Make sure to update their last-login!
+ */
+router.post("/register", async (req, res, next) => {
+    try {
+        let { username } = await User.register(req.body);
+        let token = jwt.sign({ username }, SECRET_KEY);
+        User.updateLoginTimestamp(username);
+        return res.json({ token });
+    } catch(e) {
+        // look for the postgresql error code
+        // 23505 is the duplicate username error
+        if (e.code === '23505') {
+            return next(new ExpressError("Username already exist. Please choose another username", 400));
+        }
+        return next(e)
+    }
+})
+
+module.exports = router;
